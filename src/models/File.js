ee2c14a341e5c934aa9fbad41cd7a09be9bef7fa
CesarCruz-lab <rbcesar86@gmail.com>
2021-07-02 13:03:51 -0500

    file model configurado

---
+++ src/models/File.js
@@ -0,0 +1,22 @@
+const { sequelize, Sequelize: db } = require('../config/sequelize');
+
+const model = {
+	id: {
+		type: db.UUID,
+		defaultValue: db.UUIDV4,
+		allowNull: false,
+		primaryKey: true
+	},
+	name: db.STRING,
+	key: db.STRING,
+	mimetype: db.STRING,
+	url: db.TEXT
+};
+
+const table = {
+	tableName: process.env.SEQUELIZE_TABLENAME
+};
+
+const FileModel = sequelize.define(table.tableName, model, table);
+
+module.exports = FileModel;
